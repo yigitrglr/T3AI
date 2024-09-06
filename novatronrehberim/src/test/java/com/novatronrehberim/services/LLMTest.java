6fd3225bfe4d74f0b130287d80b4b0c38c2ad9e3
yigitrglr <yigitridvanguler@gmail.com>
2024-09-06 21:14:42 +0300

    sync

--- novatronrehberim/src/test/java/com/novatronrehberim/services/LLMTest.java
+++ novatronrehberim/src/test/java/com/novatronrehberim/services/LLMTest.java
@@ -6,11 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 import java.io.PrintStream;
-import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
